cc93dac2e053438f14b2173a65f44eb06af995a5
dasturchi-2023 <pojega6913@snowlash.com>
2023-12-23 21:04:18 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,36 @@
+# n=4
+# k=7
+# count=0
+# for i in range(k,n-1,-1):
+#     print(f"i: {i}")
+#     count+=1
+# print(f"count: {count}")
+
+# price=15
+# for i in range(1,11):
+#     print(f"{i}-kg konfkt narxi: {price*i}.000.som")
+# a=int(input("a: "))
+# narxi=15
+# for i in range(1,11):
+#     print(f"{i/10}-kg konfet narxi: {i/10*narxi}.som")
+# a=2
+# narxi=15
+# for i in range(12,21,2):
+#     kilo=i/10
+#     print(f"{kilo}-kg konfet narxi: {kilo*narxi}.som")
+# a=2
+# b=11
+# summa = 1
+# for i in range(a, b+1):
+#     summa = summa * i
+# print(summa)
+# n=10
+# summa=0
+# for i in range(n,n*2):
+#     summa+=i**3
+# print(summa)
+# n=2
+# summa=1
+# for i in range(1,n+1):
+#     summa*=i/10+1
+# print(summa)
